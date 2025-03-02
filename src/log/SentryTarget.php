155e70f7b432ad2ef61cb005a05d3d681157468a
Martin Leveille <martin@diginov.com>
2025-03-01 21:12:24 -0500

    Get correct user ip

--- src/log/SentryTarget.php
+++ src/log/SentryTarget.php
@@ -134,7 +134,7 @@ class SentryTarget extends \yii\log\Target
                         'id'         => in_array('id', $this->userPrivacy) ? $user->id : null,
                         'email'      => in_array('email', $this->userPrivacy) ? $user->email : null,
                         'username'   => in_array('username', $this->userPrivacy) ? $user->username : null,
-                        'ip_address' => in_array('ip_address', $this->userPrivacy) ? $request->getRemoteIP() : null,
+                        'ip_address' => in_array('ip_address', $this->userPrivacy) ? $request->getUserIP() : null,
                         'Admin'      => in_array('permissions', $this->userPrivacy) ? ($user->admin ? 'Yes' : 'No') : null,
                         'Groups'     => in_array('permissions', $this->userPrivacy) ? ($groups ? implode(', ', $groups) : null) : null,
                     ]);
